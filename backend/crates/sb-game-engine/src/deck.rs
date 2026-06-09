a81f502a98a4c1074376e46ad1c3b95c6b62385d
elcoosp <elcoosp@gmail.com>
2026-06-09 14:13:33 +0200

    feat(game-engine): implement Deck with thread_rng

--- backend/crates/sb-game-engine/src/deck.rs
+++ backend/crates/sb-game-engine/src/deck.rs
@@ -1,7 +1,7 @@
 //! Standard 52-card deck.
 
 use rand::seq::SliceRandom;
-use rand::prelude::thread_rng;
+use rand::thread_rng;
 use sb_shared_types::{Card, Suit, Rank};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
