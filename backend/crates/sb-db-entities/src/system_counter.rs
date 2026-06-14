972a50fd26d5134079cd6c381775a1f6918b178b
elcoosp <elcoosp@gmail.com>
2026-06-14 14:59:22 +0200

    feat(db-entities): add SystemCounter entity

---
+++ backend/crates/sb-db-entities/src/system_counter.rs
@@ -0,0 +1,15 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "system_counter")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub name: String,
+    pub value: i64,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
