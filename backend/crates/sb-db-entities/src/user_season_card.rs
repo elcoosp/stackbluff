0887f9dc24ff1efd28a5a0fc936334e9d4effb95
elcoosp <elcoosp@gmail.com>
2026-06-27 18:00:25 +0200

    fix(entity): correct user_season_card types

--- backend/crates/sb-db-entities/src/user_season_card.rs
+++ backend/crates/sb-db-entities/src/user_season_card.rs
@@ -1,7 +1,8 @@
 use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize, Deserialize)]
 #[sea_orm(table_name = "user_season_cards")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
