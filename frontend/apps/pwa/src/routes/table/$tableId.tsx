33af3cd62ccfa8d96a6c86c9f2c6450531fe83bc
elcoosp <elcoosp@gmail.com>
2026-07-08 15:19:40 +0200

    fix: correct table route syntax

--- frontend/apps/pwa/src/routes/table/$tableId.tsx
+++ frontend/apps/pwa/src/routes/table/$tableId.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { TablePage } from '../../pages/TablePage';
-// import { useGameHandCompletion } from "./useGameHandCompletion";export const Route = createFileRoute('/table/$tableId')({
+
+export const Route = createFileRoute('/table/$tableId')({
   component: TablePage,
+});
