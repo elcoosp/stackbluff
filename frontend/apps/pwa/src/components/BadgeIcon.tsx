9228503a138e089d328f695a1992505fc929990b
elcoosp <elcoosp@gmail.com>
2026-07-14 17:00:08 +0200

    refactor(pwa): wrap BadgeIcon strings with Lingui macros

--- frontend/apps/pwa/src/components/BadgeIcon.tsx
+++ frontend/apps/pwa/src/components/BadgeIcon.tsx
@@ -1,4 +1,5 @@
 import { useUserBadges } from "../hooks/useBadges";
+import { t } from "@lingui/core/macro";
 
 interface BadgeIconProps {
   userId: string;
@@ -15,7 +16,7 @@ export function BadgeIcon({ userId }: BadgeIconProps) {
 
   return (
     <span
-      title="Founding Member – Referred 10 friends who played 5+ hands"
+      title={t`Founding Member – Referred 10 friends who played 5+ hands`}
       className="inline-block ml-1"
     >
       👑
