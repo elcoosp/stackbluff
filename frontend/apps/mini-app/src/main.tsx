c5064bcba6cdf1f7b95b489edebfed5581c39a35
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:45 +0200

    feat(frontend/apps): update main

--- frontend/apps/mini-app/src/main.tsx
+++ frontend/apps/mini-app/src/main.tsx
@@ -1,7 +1,7 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { routeTree } from './routeTree.gen';
 import './index.css';
 
@@ -12,7 +12,9 @@ const queryClient = new QueryClient({
 const router = createRouter({ routeTree });
 
 declare module '@tanstack/react-router' {
-  interface Register { router: typeof router; }
+  interface Register {
+    router: typeof router;
+  }
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -20,5 +22,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
