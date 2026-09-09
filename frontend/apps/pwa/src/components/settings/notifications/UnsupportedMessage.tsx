23fbb34b1e17aaee02eeb537b409e0dc5428becf
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:53 +0200

    feat(frontend/apps): update UnsupportedMessage

--- frontend/apps/pwa/src/components/settings/notifications/UnsupportedMessage.tsx
+++ frontend/apps/pwa/src/components/settings/notifications/UnsupportedMessage.tsx
@@ -4,7 +4,8 @@ export function UnsupportedMessage() {
   return (
     <div className="p-3 rounded-lg bg-yellow-500/10 border border-yellow-500/30 text-xs leading-relaxed">
       <Trans>
-        Push notifications are not supported in this browser. Try using Chrome, Firefox, or Safari 16.4+.
+        Push notifications are not supported in this browser. Try using Chrome, Firefox, or Safari
+        16.4+.
       </Trans>
     </div>
   );
