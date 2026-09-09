07a6c68d6c938e380c0964d87b865ca95881aab2
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:34 +0200

    feat(sb-auth/src): update lib

--- backend/crates/sb-auth/src/lib.rs
+++ backend/crates/sb-auth/src/lib.rs
@@ -20,6 +20,5 @@ pub use auth_service::Authenticator; // we'll add this trait in auth_service.rs
 
 pub type SharedAuthService = std::sync::Arc<dyn AuthService + Send + Sync>;
 
-
 #[cfg(test)]
 mod tests;
