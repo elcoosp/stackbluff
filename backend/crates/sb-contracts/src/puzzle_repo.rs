1e3acc0ac0079c2578951426c9b93e034107e976
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:36 +0200

    feat(sb-contracts/src): update puzzle_repo

--- backend/crates/sb-contracts/src/puzzle_repo.rs
+++ backend/crates/sb-contracts/src/puzzle_repo.rs
@@ -10,6 +10,10 @@ pub enum PuzzleError {
 
 #[async_trait::async_trait]
 pub trait PuzzleRepo: Send + Sync {
-    async fn find_submission(&self, user_id: Uuid, date: NaiveDate) -> Result<Option<PuzzleSubmissionRecord>, PuzzleError>;
+    async fn find_submission(
+        &self,
+        user_id: Uuid,
+        date: NaiveDate,
+    ) -> Result<Option<PuzzleSubmissionRecord>, PuzzleError>;
     async fn save_submission(&self, record: PuzzleSubmissionRecord) -> Result<(), PuzzleError>;
 }
