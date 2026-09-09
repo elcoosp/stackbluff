2b0e8bdda9e469b79c2a1d5111cfa4388be22a90
elcoosp <elcoosp@gmail.com>
2026-09-09 23:30:09 +0200

    feat(frontend/packages): update index

--- frontend/packages/shared/src/index.ts
+++ frontend/packages/shared/src/index.ts
@@ -1,10 +1,9 @@
-export * from './platform';
-export * from './stores';
-
-export * from './api/client';export * from './api/client';
-
-// Tournament
-export * from './types/tournament.types';
-export * from './stores/tournamentStore';
+export * from './api/client';
+export * from './api/client';
 export * from './api/tournamentApi';
 export * from './hooks/useTournament';
+export * from './platform';
+export * from './stores';
+export * from './stores/tournamentStore';
+// Tournament
+export * from './types/tournament.types';
