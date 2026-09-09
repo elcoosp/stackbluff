e56fccc4ec8498b87f71b695445f78b3f2f0af04
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:42 +0200

    feat(migration): m20260712_000001_create_analytics_events.rs

--- backend/migration/src/m20260712_000001_create_analytics_events.rs
+++ backend/migration/src/m20260712_000001_create_analytics_events.rs
@@ -17,11 +17,7 @@ impl MigrationTrait for Migration {
                             .not_null()
                             .primary_key(),
                     )
-                    .col(
-                        ColumnDef::new(AnalyticsEvents::UserId)
-                            .uuid()
-                            .not_null(),
-                    )
+                    .col(ColumnDef::new(AnalyticsEvents::UserId).uuid().not_null())
                     .col(
                         ColumnDef::new(AnalyticsEvents::EventType)
                             .string()
