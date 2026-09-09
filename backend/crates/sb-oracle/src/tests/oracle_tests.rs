5f075164eba4f7dba5ce2c496bf2e422c2030afd
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:38 +0200

    feat(sb-oracle/src/tests): update oracle_tests

--- backend/crates/sb-oracle/src/tests/oracle_tests.rs
+++ backend/crates/sb-oracle/src/tests/oracle_tests.rs
@@ -26,7 +26,7 @@ mock! {
         async fn is_email_verified(&self, ctx: RequestContext, user_id: UserId) -> Result<bool, PersistenceError>;
         async fn has_active_season_pass(&self, ctx: RequestContext, user_id: UserId) -> Result<bool, PersistenceError>;
         async fn find_by_telegram(&self, ctx: RequestContext, tg_id: i64) -> Result<Option<UserId>, PersistenceError>;
-    
+
 
         async fn extend_season_pass(
             &self,
