9e4ea2a8c04a68b3bd1dcc155433c2b167f807ec
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:36 +0200

    feat(sb-db-entities/src): update lib

--- backend/crates/sb-db-entities/src/lib.rs
+++ backend/crates/sb-db-entities/src/lib.rs
@@ -26,11 +26,11 @@ pub mod user_statistics;
 pub use referral::Entity as Referral;
 pub use system_counter::Entity as SystemCounter;
 pub use user_season_card::Entity as UserSeasonCard;
-pub mod deletion_request;
-pub mod puzzle_submission;
-pub mod user_season_card;
 pub mod analytics_event;
+pub mod deletion_request;
 pub mod products;
+pub mod puzzle_submission;
 pub mod user_notification_preferences;
+pub mod user_season_card;
 
 pub mod push_subscription;
