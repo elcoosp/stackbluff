b94c44a391b93cb6206f332b308fc690c9baf94d
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:41 +0200

    feat(sb-viral/src/puzzle): update data

--- backend/crates/sb-viral/src/puzzle/data.rs
+++ backend/crates/sb-viral/src/puzzle/data.rs
@@ -3,12 +3,13 @@ use chrono::{Datelike, Utc};
 use once_cell::sync::Lazy;
 
 static PUZZLES_JSON: &str = include_str!("../../assets/puzzles.json");
-static PUZZLES: Lazy<Vec<Puzzle>> = Lazy::new(|| {
-    serde_json::from_str(PUZZLES_JSON).expect("FATAL: Corrupted puzzles.json")
-});
+static PUZZLES: Lazy<Vec<Puzzle>> =
+    Lazy::new(|| serde_json::from_str(PUZZLES_JSON).expect("FATAL: Corrupted puzzles.json"));
 
 pub fn get_today_puzzle() -> Option<&'static Puzzle> {
     let days = Utc::now().date_naive().num_days_from_ce() as usize;
     PUZZLES.get(days % PUZZLES.len())
 }
-pub fn get_today_date() -> chrono::NaiveDate { Utc::now().date_naive() }
+pub fn get_today_date() -> chrono::NaiveDate {
+    Utc::now().date_naive()
+}
