40e1342d0e0994430dcef97ce3eaad672018a63a
elcoosp <elcoosp@gmail.com>
2026-09-09 23:30:01 +0200

    feat(frontend/apps): update ProfilePage

--- frontend/apps/pwa/src/pages/ProfilePage.tsx
+++ frontend/apps/pwa/src/pages/ProfilePage.tsx
@@ -1,10 +1,12 @@
-import { BadgeDisplay } from '../components/BadgeDisplay';
 import { Trans } from '@lingui/react/macro';
+import { BadgeDisplay } from '../components/BadgeDisplay';
 
 export function ProfilePage() {
   return (
     <div className="profile-page">
-      <h1><Trans>Profile</Trans></h1>
+      <h1>
+        <Trans>Profile</Trans>
+      </h1>
       <BadgeDisplay showProgress={true} />
     </div>
   );
