77ea7370bba800a3c36f9ab35c90ed70ac54670b
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:51 +0200

    feat(frontend/apps): update TableFelt

--- frontend/apps/pwa/src/components/game/TableFelt.tsx
+++ frontend/apps/pwa/src/components/game/TableFelt.tsx
@@ -62,7 +62,8 @@ export const TableFelt = ({ isMobile, ...props }: { isMobile?: boolean }) => {
 
   return (
     <div
-      className="absolute inset-0 z-[1]" data-club-felt
+      className="absolute inset-0 z-[1]"
+      data-club-felt
       style={{
         borderRadius: r,
         background: gradient,
