737dcdfb5bc83b7405110a852bc83bad837703d9
elcoosp <elcoosp@gmail.com>
2026-09-09 23:29:45 +0200

    feat(frontend/apps): update __root

--- frontend/apps/mini-app/src/routes/__root.tsx
+++ frontend/apps/mini-app/src/routes/__root.tsx
@@ -1,5 +1,5 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { Header } from '@stackbluff/shared/components/Header';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
 
 export const Route = createRootRoute({
   component: () => (
