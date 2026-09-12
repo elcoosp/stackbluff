0b8218c09df85e1ccd6e34a8e9220af374d548b1
elcoosp <elcoosp@gmail.com>
2026-09-12 12:12:51 +0200

    fix(pwa): add empty alt to AvatarImage

--- frontend/apps/pwa/src/components/ui/avatar.tsx
+++ frontend/apps/pwa/src/components/ui/avatar.tsx
@@ -13,7 +13,7 @@ const Avatar = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElem
 Avatar.displayName = 'Avatar';
 const AvatarImage = React.forwardRef<HTMLImageElement, React.ImgHTMLAttributes<HTMLImageElement>>(
   ({ className, ...props }, ref) => (
-    <img ref={ref} className={cn('aspect-square h-full w-full', className)} {...props} />
+    <img ref={ref} alt="" className={cn('aspect-square h-full w-full', className)} {...props} />
   ),
 );
 AvatarImage.displayName = 'AvatarImage';
